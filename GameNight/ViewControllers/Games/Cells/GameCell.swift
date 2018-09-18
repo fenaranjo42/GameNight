8e84eda6ed5de6278ea5905bb1b30a6fcf7b883e
Felipe Naranjo <felipe@Felipes-MacBook-Pro-2.local>
2018-09-18 16:35:46 -0400

    ---

--- GameNight/ViewControllers/Games/Cells/GameCell.swift
+++ GameNight/ViewControllers/Games/Cells/GameCell.swift
@@ -1,6 +1,6 @@
 //
 //  GameCell.swift
-//  Fanduel-Challenge
+//  GameNight
 //
 //  Created by Felipe Naranjo on 9/17/18.
 //  Copyright © 2018 Felipe Naranjo. All rights reserved.
--- GameNight/ViewControllers/Games/GamesViewController.swift
+++ GameNight/ViewControllers/Games/GamesViewController.swift
@@ -1,6 +1,6 @@
 //
 //  GamesViewController.swift
-//  Fanduel-Challenge
+//  GameNight
 //
 //  Created by Felipe Naranjo on 9/17/18.
 //  Copyright © 2018 Felipe Naranjo. All rights reserved.
